1069fefe32e7657fa78c9e4d823a91425289c333
Daniel Panayotov <dani.dani2004@abv.bg>
2025-11-30 18:23:36 +0200

    migrations and some css

---
+++ semester-5/book-store/Migrations/20251130162046_initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace book_store.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookISBN = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CoverLink = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Pages = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Books__3DE0C2277D470D70", x => x.BookID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    OrderTime = table.Column<DateTime>(type: "datetime", nullable: false),
+                    TotalPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Orders__C3905BAFB8B0C7CC", x => x.OrderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "relOrderBooks",
+                columns: table => new
+                {
+                    relOrderBooksID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    BookID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__relOrder__87FB36DA059F26EA", x => x.relOrderBooksID);
+                    table.ForeignKey(
+                        name: "FK__relOrderB__BookI__3C69FB99",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "BookID");
+                    table.ForeignKey(
+                        name: "FK__relOrderB__Order__3B75D760",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_relOrderBooks_BookID",
+                table: "relOrderBooks",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_relOrderBooks_OrderID",
+                table: "relOrderBooks",
+                column: "OrderID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "relOrderBooks");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
